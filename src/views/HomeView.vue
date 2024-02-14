5a01ba1a6743bc11b97c1d9a1dc760537f9ff3f1
Antonio Pontes <developer.antonio.pontes@gmail.com>
2024-02-14 22:05:11 +0000

    feat: pokemons list
    WIP: rendering pokemons list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Data Fetching Example</h1>
+    <ul>
+      <li v-for="(pokemon, index) in pokemons" :key="index">
+        <a :href="pokemon.url">{{ pokemon.name }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const pokemons = ref([
+  {
+    name: String,
+    url: String
+  }
+])
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+    pokemons.value = response.data.results
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(async () => {
+  await fetchData()
+})
+</script>
